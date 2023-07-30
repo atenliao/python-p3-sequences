4094637b1e30935999bd9a450c59caa232dcde5b
zhongming <atenliao@gmail.com>
2023-07-29 20:49:44 -0700

    complete sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    if(length == 0):
+        print(fib)
+    elif(length== 1):
+        fib.append(0)
+        print(fib)
+    else: 
+        fib.append(0)
+        fib.append(1)
+        newElement = 0
+        for i in range(length-2):
+            # print(f'i: {i}')
+            newElement = fib[i] + fib[i+1]
+            fib.append(newElement)
+            # print(f'fib: {fib}')
+    
+        print(fib)
